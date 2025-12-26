407c542758d21d5ee5bea5299068e6f7d00db539
tamal006 <tamalkumarkhan006@gmail.com>
2025-12-26 20:55:22 +0530

    Update for deployment

--- backend/server.js
+++ backend/server.js
@@ -6,17 +6,38 @@ import connectDB from './config/db.js';
 import userRouter from './routes/userAuth.js';
 import resumeRouter from './routes/resumeData.js';
 import resumeToken from './middleware/resumeToken.js';
+import { mongoose } from 'mongoose';
 const app = express()
 const port = process.env.PORT||3000
 
 app.use(express.json());
 app.use(cors());
-connectDB();
+// connectDB();
+let isConnected=false;
+async function dbConnection(){
+  try {
+    await mongoose.connect(process.env.MONGO_URL,{
+      useNewUrlParser:true,
+      useUnifiedTopology:true,
+    });
+    isConnected=true;
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+  }
+}
+app.use((req,res,next)=>{
+  if(!isConnected){
+     dbConnection();
+  }
+  next();
+})
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 app.use('/api/user',userRouter)
 app.use('/api/resume',resumeToken,resumeRouter)
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
+export default app;
