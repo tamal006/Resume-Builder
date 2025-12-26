2e36ca79ac7b577178c98548c70e7b01b80efaa6
tamal006 <tamalkumarkhan006@gmail.com>
2025-12-26 21:27:34 +0530

    changes for deployment

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,10 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from "dotenv";
 dotenv.config();
-import connectDB from './config/db.js';
 import userRouter from './routes/userAuth.js';
 import resumeRouter from './routes/resumeData.js';
 import resumeToken from './middleware/resumeToken.js';
-import { mongoose } from 'mongoose';
+import mongoose from "mongoose";
 const app = express()
 const port = process.env.PORT||3000
 
@@ -16,22 +15,21 @@ app.use(cors());
 let isConnected=false;
 async function dbConnection(){
   try {
-    await mongoose.connect(process.env.MONGO_URL,{
-      useNewUrlParser:true,
-      useUnifiedTopology:true,
-    });
+    await mongoose.connect(process.env.MONGO_URL);
     isConnected=true;
     console.log("MongoDB connected");
   } catch (error) {
     console.error("MongoDB connection error:", error);
   }
 }
-app.use((req,res,next)=>{
-  if(!isConnected){
-     dbConnection();
+app.use(async (req, res, next) => {
+  try {
+    await dbConnection();
+    next();
+  } catch (err) {
+    res.status(500).json({ message: "Database connection failed" });
   }
-  next();
-})
+});
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
