0c541b81b34dafaa6f97f083cb2423b8cce1add7
tamal006 <tamalkumarkhan006@gmail.com>
2026-07-10 22:34:09 +0530

    Change server port from 3000 to 5000

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ import resumeRouter from './routes/resumeData.js';
 import resumeToken from './middleware/resumeToken.js';
 import mongoose from "mongoose";
 const app = express()
-const port = process.env.PORT||3000
+const port = process.env.PORT||5000
 
 app.use(express.json());
 app.use(cors());
